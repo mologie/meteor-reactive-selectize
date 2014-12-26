422bb809d2aea0fc4918710478cd490dd1184b8f
Oliver Kuckertz <oliver.kuckertz@mologie.de>
2014-12-26 23:11:55 +0100

    Fix meteor version

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-	api.versionsFrom('1.0.0');
+	api.versionsFrom('1.0.2.1');
 	api.use('coffeescript');
 	api.use('jquery');
 	api.addFiles('reactive-selectize.coffee', 'client');
